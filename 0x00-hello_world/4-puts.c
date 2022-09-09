f0bb1c9cebfd75f9ba76b46d41012dcb26f71cae
yemreel <110992608+yemreel@users.noreply.github.com>
2022-09-09 03:59:29 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
-4-puts.c
 #include <stdio.h>
 /**
-* main - main block
-* Return: 0
-*    */
+* main - Entry point
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
