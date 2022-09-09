0999695f27c63ad5eabba8ea4fe33961423c09fd
yemreel <yemreel02@gmail.com>
2022-09-08 19:37:45 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+4-puts.c
+#include <stdio.h>
+/**
+ *  * main - main block
+ *   * Return: 0
+ *    */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
+
