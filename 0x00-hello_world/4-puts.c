cafc3d78245c8233c92fbcf3b99974129bcb9e8b
yemreel <110992608+yemreel@users.noreply.github.com>
2022-09-09 03:51:03 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 4-puts.c
 #include <stdio.h>
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+*  * main - main block
+*   * Return: 0
+*    */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
