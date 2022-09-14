303a749fa42a549ebfe167e1bbd458fae779e834
yemreel <110992608+yemreel@users.noreply.github.com>
2022-09-14 10:00:39 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,7 +11,8 @@ int j;
 for (i = 0; i <= 23; i++)
 {
 for (j = 0; j <= 59; j++)
-{ _putchar(i / 10 + '0');
+{ 
+_putchar(i / 10 + '0');
 _putchar(i % 10 + '0');
 _putchar(':');
 _putchar(j / 10 + '0');
