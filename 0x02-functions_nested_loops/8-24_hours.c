669f5cc9434155240af85b58ff1fdee084f32cc1
yemreel <110992608+yemreel@users.noreply.github.com>
2022-09-14 10:04:47 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,22 +1,22 @@
-#include "main.h"
+#include"main.h"
+
 /**
- * jack_bauer - main thing
- *
- */
+* jack_bauer- prints every minute of the day
+*/
+
 void jack_bauer(void)
 {
-int i;
-int j;
+int min, hr;
 
-for (i = 0; i <= 23; i++)
+for (hr = 0; hr <= 23; ++hr)
 {
-for (j = 0; j <= 59; j++)
-{ 
-_putchar(i / 10 + '0');
-_putchar(i % 10 + '0');
+for (min = 0; min <= 59; ++min)
+{
+_putchar((hr / 10) + 48);
+_putchar((hr % 10) + 48);
 _putchar(':');
-_putchar(j / 10 + '0');
-_putchar(j % 10 + '0');
+_putchar((min / 10) + 48);
+_putchar((min % 10) + 48);
 _putchar('\n');
 }
 }
