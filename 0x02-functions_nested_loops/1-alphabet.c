552dfdbdbf877fcb6cfd77aa9309901975cc94bd
yemreel <yemreel02@gmail.com>
2022-09-14 00:37:30 -0700

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print english letters
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char i
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar(10);
+}
