09881d9326e8347007e896f2e2ff26bdc6797ffd
yemreel <110992608+yemreel@users.noreply.github.com>
2022-09-14 08:47:04 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
-#include "main.h" 
-  
- /** 
-  ** print_alphabet - print english letters 
-  ** Return: void 
-  **/
-  
- void print_alphabet(void)
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Return: always 0
+ *
+ */
+void print_alphabet(void)
 {
-char i
+char i;
+
 for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
 }
-_putchar(10);
+_putchar('\n');
 }
