a424071c664a6b3a9c9b33c9f1da350a784e6a33
yemreel <110992608+yemreel@users.noreply.github.com>
2022-09-15 04:12:59 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,32 +1,12 @@
 #include "main.h"
-#include <stdio.h>
-#include "6-abs.c"
 /**
-* print_to_98 - prints to 98
-* @n: parameter
+* add - adding it up
+* @i: first integer
+* @j: second integer
 *
-* Return: always returns nothing
+* Return: sum of both integers
 */
-void print_to_98(int n);
+int add(int i, int j)
 {
-int i;
-
-if (n > 98)
-{
-for (i = n; i >= 98;  i--);
-{
-printf("%d", i);
-if (i != 98)
-{
-printf(", ");
-}
-}
-else
-{
-for (i = n; i <= 98; i++)
-{
-printf("%d", i);
-if (i != 98)
-{
-printf(", ");
+return (i + j);
 }
