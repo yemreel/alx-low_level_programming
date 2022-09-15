b45dac1e45c0e7bbbb2685ccad54f74da8439ac9
yemreel <yemreel02@gmail.com>
2022-09-14 19:16:57 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include "6-abs.c"
+/**
+ * print_to_98 - prints to 98
+ * @n: parameter
+ *
+ * Return: always returns nothing
+ */
+
+void print_to_98(int n)
+{
+int i;
+
+if (n > 98)
+{
+for (i = n; i >= 98;  i--)
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
+}
+}
+}
+else
+{
+for (i = n; i <= 98; i++)
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
+}
