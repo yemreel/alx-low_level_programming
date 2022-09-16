7ce328ecca18b60cd56f47d142fcabdb71ff02a8
yemreel <110992608+yemreel@users.noreply.github.com>
2022-09-16 05:26:20 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,13 +3,16 @@
 * print_most_numbers - prints numbers from 0 to 9 with a new line
 *
 */
+void print_most_numbers(void)
+{
 int i;
 
 for (i = '0'; i <= '9'; i++)
 {
-if (i != '2' && i != '4')
+if (i != '2' &&  i != '4')
 {
 _putchar(i);
 }
 }
-_purchar('\n');
+_putchar('\n');
+}
