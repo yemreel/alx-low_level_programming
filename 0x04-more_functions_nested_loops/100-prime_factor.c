3d79d3777e6a9994911b15c7133f812343dde5ff
yemreel <yemreel02@gmail.com>
2022-09-16 17:36:14 -0700

    The problem of distinguishing prime numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - main function
+*
+* Return: always return 0
+*/
+int main(void)
+{
+unsigned int 1 = 2;
+unsigned long n = 612852475143;
+
+while (i !=n)
+{
+if (n % i == 0)
+{
+n = n / i;
+}
+else
+{
+i++;
+}
+}
+printf("%lu\n", n);
+return (0);
+}
