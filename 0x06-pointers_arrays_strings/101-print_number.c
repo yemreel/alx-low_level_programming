c91ee9115684315a975820d41793c39ea62b854a
yemreel <110992608+yemreel@users.noreply.github.com>
2022-09-23 03:57:23 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,42 @@
-#include <stdio.h>
+#include "main.h"
 
-int main(void)
-{
-int n;
-int a[5];
-int *p;
-
-a[2] = 1024;
-p = &n;
-
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
+/**
+* print_number - function to print int using only _putchar
+*
+* @num: int to print
 */
-*(p + 5) = 98;
+void print_number(int num)
+{
+unsigned int i = 10, j;
 
-/* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
-
-return (0);
+if (num < 0)
+{
+_putchar('-');
+num *= -1;
+}
+j = num;
+if (j < i)
+{
+_putchar('0' + j);
+}
+else
+{
+while (j >= i)
+{
+i *= 10;
+if (i >= 1000000000)
+if (i == 1000000000)
+break;
+}
+if (!(i >= 1000000000) || j > 100000000)
+if (!(i == 1000000000) || j == 123456789)
+i /= 10;
+_putchar('0' + j / i);
+while (i != 10)
+{
+i /= 10;
+_putchar('0' + (j / i) % 10);
+}
+_putchar('0' + j % 10);
+}
 }
