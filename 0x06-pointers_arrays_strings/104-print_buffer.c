9dcdb1c2f5ba87fbd3da79465840d6c8be13cc70
yemreel <yemreel02@gmail.com>
2022-09-23 22:01:22 -0700

    Noise is a buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_buffer - Function that prints a buffer Prints a buffer
+*
+* @b: The buffer to be printed.
+*
+* @size: The number of bytes to be printed from the buffer.
