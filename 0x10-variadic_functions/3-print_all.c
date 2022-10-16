55b736ebf200ebdc5427234f8b035d7e97764e3d
yemreel <yemreel02@gmail.com>
2022-10-16 08:59:25 -0700

    To be is a to be the value of a variable

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+void print_all(const char * const format, ...);
+
+/**
+* print_char - Prints a char.
